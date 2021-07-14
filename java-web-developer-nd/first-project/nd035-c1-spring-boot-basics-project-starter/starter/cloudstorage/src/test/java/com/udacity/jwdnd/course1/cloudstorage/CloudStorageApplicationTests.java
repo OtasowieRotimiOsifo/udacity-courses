20df69b3bd975f5c50f77a4613ba79ec9cb28340
Otasowie Osifo <Otasowie.Osifo@physics.org>
2021-07-14 18:41:16 +0200

    started work on tests

--- java-web-developer-nd/first-project/nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ java-web-developer-nd/first-project/nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -2,19 +2,29 @@ package com.udacity.jwdnd.course1.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
+	Logger logger = LoggerFactory.getLogger(CloudStorageApplicationTests.class);
+	
 	@LocalServerPort
 	private int port;
 
 	private WebDriver driver;
 
+	public String baseURL;
+	
 	@BeforeAll
 	static void beforeAll() {
 		WebDriverManager.chromedriver().setup();
@@ -23,6 +33,8 @@ class CloudStorageApplicationTests {
 	@BeforeEach
 	public void beforeEach() {
 		this.driver = new ChromeDriver();
+		
+	    baseURL = "http://localhost:" + port;
 	}
 
 	@AfterEach
@@ -34,8 +46,98 @@ class CloudStorageApplicationTests {
 
 	@Test
 	public void getLoginPage() {
-		driver.get("http://localhost:" + this.port + "/login");
+		driver.get( baseURL  + "/login");
+		Assertions.assertEquals("Login", driver.getTitle());
+	}
+	
+	@Test
+	public void getSignupPage() {
+		driver.get( baseURL  + "/signup");
+		Assertions.assertEquals("Sign Up", driver.getTitle());
+	}
+	
+	@Test
+	public void getLogoutRedirect() {
+		driver.get( baseURL  + "/login?logout");
+		Assertions.assertEquals("Login", driver.getTitle());
+	}
+	
+	@Test
+	public void getLandingPage() {
+		driver.get("http://localhost:" + this.port);
 		Assertions.assertEquals("Login", driver.getTitle());
 	}
 
+	@Test
+	public void getUserSignup()  {
+		
+		driver.get(baseURL + "/signup");
+
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup(UserData.firstname, UserData.lastname, UserData.username, UserData.password);
+		
+		WebElement loginLink = driver.findElement(By.xpath("//a[contains(text(),'Login')]"));
+    	loginLink.click(); //redirection after signup
+    	
+		String currentUrl = driver.getCurrentUrl();
+		driver.get(currentUrl);
+		Assertions.assertEquals("Login", driver.getTitle()); 
+	}
+	
+	@Test
+	public void getSignupAndLogin() {
+		driver.get(baseURL + "/signup");
+
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup(UserData.firstname, UserData.lastname, UserData.username, UserData.password);
+		
+		WebElement loginLink = driver.findElement(By.xpath("//a[contains(text(),'Login')]"));
+    	loginLink.click(); //redirection after signup
+    	
+		String currentUrl = driver.getCurrentUrl();
+		driver.get(currentUrl);
+		Assertions.assertEquals("Login", driver.getTitle()); 
+		
+		//login from the login page
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(UserData.username, UserData.password);
+		
+		currentUrl = driver.getCurrentUrl();
+		driver.get(currentUrl);
+		logger.info("currentUrl: {}", currentUrl);
+		Assertions.assertEquals("Home", driver.getTitle()); 
+	}
+	
+	@Test
+	public void getSignupAndLoginAndLogout() {
+		driver.get(baseURL + "/signup");
+
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup(UserData.firstname, UserData.lastname, UserData.username, UserData.password);
+		
+		WebElement loginLink = driver.findElement(By.xpath("//a[contains(text(),'Login')]"));
+    	loginLink.click(); //redirection after signup
+    	
+		String currentUrl = driver.getCurrentUrl();
+		driver.get(currentUrl);
+		Assertions.assertEquals("Login", driver.getTitle()); 
+		
+		//login from the login page
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(UserData.username, UserData.password);
+		
+		currentUrl = driver.getCurrentUrl();
+		driver.get(currentUrl);
+		logger.info("currentUrl: {}", currentUrl);
+		Assertions.assertEquals("Home", driver.getTitle()); 
+		
+		//logout
+		LogoutPage logoutPage = new LogoutPage(driver);
+		logoutPage.logout();
+		
+		currentUrl = driver.getCurrentUrl();
+		driver.get(currentUrl);
+		logger.info("currentUrl: {}", currentUrl);
+		Assertions.assertEquals("Login", driver.getTitle()); 
+	}
 }
---
+++ java-web-developer-nd/first-project/nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/UserData.java
@@ -0,0 +1,8 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+public class UserData {
+	public static String username = "star";
+	public static String password = "alpha-centuri";
+	public static String firstname = "star";
+	public static String lastname = "star";
+}
