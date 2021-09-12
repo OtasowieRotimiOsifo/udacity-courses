bf6519652c8ec190881bef415c413ec6fd2f8743
Otasowie Osifo <Otasowie.Osifo@physics.org>
2021-09-12 21:31:30 +0200

    Added registration of price service with eureka

--- java-web-developer-nd/nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ java-web-developer-nd/nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,12 +1,20 @@
 package com.udacity.pricing.domain.price;
 
 import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
 public class Price {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+	
     private String currency;
     private BigDecimal price;
     private Long vehicleId;
@@ -14,6 +22,14 @@ public class Price {
     public Price() {
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
     public Price(String currency, BigDecimal price, Long vehicleId) {
         this.currency = currency;
         this.price = price;
--- java-web-developer-nd/nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ java-web-developer-nd/nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -1,8 +1,8 @@
 package com.udacity.pricing.domain.price;
 
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
-@Repository
-public class PriceRepository {
-
+//@Repository
+public interface  PriceRepository  extends CrudRepository<Price, Long> {
 }
