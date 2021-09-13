f258023874b464e77c4e8f1facbd620d90f0e014
Otasowie Osifo <Otasowie.Osifo@physics.org>
2021-09-13 22:21:06 +0200

    added new functions for pricing service

--- java-web-developer-nd/nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ java-web-developer-nd/nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -6,57 +6,27 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 @Entity
 public class Price {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-	
-    private String currency;
-    private BigDecimal price;
-    private Long vehicleId;
-
-    public Price() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
-    public Price(String currency, BigDecimal price, Long vehicleId) {
-        this.currency = currency;
-        this.price = price;
-        this.vehicleId = vehicleId;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Long getVehicleId() {
-        return vehicleId;
-    }
-
-    public void setVehicleId(Long vehicleId) {
-        this.vehicleId = vehicleId;
-    }
+    private @Getter @Setter Long id;
+    private @Getter @Setter String currency;
+    private @Getter @Setter BigDecimal price;
+    private @Getter @Setter Long vehicleId;
+   
 }
