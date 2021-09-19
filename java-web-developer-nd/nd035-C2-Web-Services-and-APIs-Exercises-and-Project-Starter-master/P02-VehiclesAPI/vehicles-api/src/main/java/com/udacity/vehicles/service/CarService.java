70ed9270eac0604056d05e2bf0f62860bb207fbf
Otasowie Osifo <Otasowie.Osifo@physics.org>
2021-09-19 20:05:40 +0200

    started the implementation of vehicle-api

--- java-web-developer-nd/nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ java-web-developer-nd/nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -1,10 +1,16 @@
 package com.udacity.vehicles.service;
 
+import com.udacity.vehicles.client.maps.MapsClient;
+import com.udacity.vehicles.client.prices.Price;
+import com.udacity.vehicles.client.prices.PriceClient;
+import com.udacity.vehicles.domain.Location;
 import com.udacity.vehicles.domain.car.Car;
 import com.udacity.vehicles.domain.car.CarRepository;
 import java.util.List;
-import org.springframework.stereotype.Service;
 
+import org.springframework.stereotype.Service;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.reactive.function.client.WebClient;
 /**
  * Implements the car service create, read, update or delete
  * information about vehicles, as well as gather related
@@ -14,12 +20,14 @@ import org.springframework.stereotype.Service;
 public class CarService {
 
     private final CarRepository repository;
-
-    public CarService(CarRepository repository) {
-        /**
-         * TODO: Add the Maps and Pricing Web Clients you create
-         *   in `VehiclesApiApplication` as arguments and set them here.
-         */
+    
+    private MapsClient mapsClient;
+    
+    private PriceClient priceClient;
+    
+    public CarService(CarRepository repository, WebClient maps, WebClient prices, ModelMapper mapper) {
+    	this.mapsClient = new MapsClient(maps, mapper);
+    	this.priceClient = new PriceClient(prices);
         this.repository = repository;
     }
 
@@ -37,32 +45,15 @@ public class CarService {
      * @return the requested car's information, including location and price
      */
     public Car findById(Long id) {
-        /**
-         * TODO: Find the car by ID from the `repository` if it exists.
-         *   If it does not exist, throw a CarNotFoundException
-         *   Remove the below code as part of your implementation.
-         */
-        Car car = new Car();
-
-        /**
-         * TODO: Use the Pricing Web client you create in `VehiclesApiApplication`
-         *   to get the price based on the `id` input'
-         * TODO: Set the price of the car
-         * Note: The car class file uses @transient, meaning you will need to call
-         *   the pricing service each time to get the price.
-         */
-
-
-        /**
-         * TODO: Use the Maps Web client you create in `VehiclesApiApplication`
-         *   to get the address for the vehicle. You should access the location
-         *   from the car object and feed it to the Maps service.
-         * TODO: Set the location of the vehicle, including the address information
-         * Note: The Location class file also uses @transient for the address,
-         * meaning the Maps service needs to be called each time for the address.
-         */
-
-
+    	Car car = this.repository.findById(id).orElseThrow(CarNotFoundException :: new);
+       
+       
+        String priceStr = this.priceClient.getPrice(car.getId());
+        car.setPrice(priceStr);
+       
+        Location address = this.mapsClient.getAddress(car.getLocation());
+        car.setLocation(address);
+       
         return car;
     }
 
@@ -89,16 +80,8 @@ public class CarService {
      * @param id the ID number of the car to delete
      */
     public void delete(Long id) {
-        /**
-         * TODO: Find the car by ID from the `repository` if it exists.
-         *   If it does not exist, throw a CarNotFoundException
-         */
-
-
-        /**
-         * TODO: Delete the car from the repository.
-         */
-
-
+    	Car car = this.repository.findById(id).orElseThrow(CarNotFoundException :: new);
+    	
+    	this.repository.deleteById(id); 	
     }
 }
