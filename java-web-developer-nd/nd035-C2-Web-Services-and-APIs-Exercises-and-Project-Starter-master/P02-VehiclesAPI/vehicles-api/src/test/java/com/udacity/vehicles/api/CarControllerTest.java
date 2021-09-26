5adb00e080d41dfe96756b97801a9b5c1f67a5c5
Otasowie Osifo <Otasowie.Osifo@physics.org>
2021-09-26 16:33:54 +0200

    Implemented the test cases for the car controller

--- java-web-developer-nd/nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ java-web-developer-nd/nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -16,14 +16,22 @@ import com.udacity.vehicles.client.prices.PriceClient;
 import com.udacity.vehicles.domain.Condition;
 import com.udacity.vehicles.domain.Location;
 import com.udacity.vehicles.domain.car.Car;
+import com.udacity.vehicles.domain.car.CarRepository;
 import com.udacity.vehicles.domain.car.Details;
 import com.udacity.vehicles.domain.manufacturer.Manufacturer;
 import com.udacity.vehicles.service.CarService;
+
+import lombok.extern.slf4j.Slf4j;
+
 import java.net.URI;
 import java.util.Collections;
+import java.util.Random;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -41,6 +49,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
+@Slf4j
 public class CarControllerTest {
 
     @Autowired
@@ -58,16 +67,31 @@ public class CarControllerTest {
     @MockBean
     private MapsClient mapsClient;
 
+    private Car carInstance = null;
+    
+    private Long carId = null;
     /**
      * Creates pre-requisites for testing, such as an example car.
      */
     @Before
     public void setup() {
-        Car car = getCar();
-        car.setId(1L);
-        given(carService.save(any())).willReturn(car);
-        given(carService.findById(any())).willReturn(car);
-        given(carService.list()).willReturn(Collections.singletonList(car));
+    
+    	Random ran = new Random();
+    	carId = ran.nextInt(100) + 100L;
+    	
+    	Location location = new Location(40.730610, -73.935242);
+    	location.setCity("New York");
+    	location.setState("New York");
+    	location.setZip("1 200 200 111 NY");
+    	Mockito.when(mapsClient.getAddress(location)).thenReturn(location);
+    	
+    	Mockito.when(priceClient.getPrice(carId)).thenReturn("2000USD");
+    
+    	carInstance = getCar(carId, location);
+        
+        given(carService.save(any())).willReturn(carInstance);
+        given(carService.findById(any())).willReturn(carInstance);
+        given(carService.list()).willReturn(Collections.singletonList(carInstance));
     }
 
     /**
@@ -89,15 +113,31 @@ public class CarControllerTest {
      * Tests if the read operation appropriately returns a list of vehicles.
      * @throws Exception if the read operation of the vehicle list fails
      */
-    @Test
-    public void listCars() throws Exception {
-        /**
-         * TODO: Add a test to check that the `get` method works by calling
-         *   the whole list of vehicles. This should utilize the car from `getCar()`
-         *   below (the vehicle will be the first in the list).
-         */
-
-    }
+	@Test
+	public void listCars() throws Exception {
+		
+		Car actual = carService.list().get(0);
+		Car expected = carInstance;
+		
+		log.info("city = {}", actual.getLocation().getCity());
+		log.info("car price = {}", actual.getPrice());
+		log.info("car model = {}", actual.getDetails().getModel());
+		log.info("Id = {}", actual.getId());
+		
+		Assert.assertEquals(actual, expected);
+		
+		 mvc.perform(get(new URI("/cars"))
+	                .accept(MediaType.APPLICATION_JSON_UTF8))
+	                .andExpect(status().isOk())
+	                .andExpect(jsonPath("$._embedded").exists())
+	                .andExpect(jsonPath("$._embedded.carList", hasSize(1)))
+	                .andExpect(jsonPath("$._embedded.carList[0].id", is(carId.intValue())))
+	                .andExpect(jsonPath("$._embedded.carList[0].price", is("2000USD")))
+	                .andExpect(jsonPath("$._embedded.carList[0].location.city", is("New York")))
+	                .andExpect(jsonPath("$._embedded.carList[0].location.state", is("New York")))
+	                .andExpect(jsonPath("$._embedded.carList[0].location.zip", is("1 200 200 111 NY")))
+	                .andExpect(jsonPath("$._embedded.carList[0].condition", is(Condition.USED.name())));
+	}
 
     /**
      * Tests the read operation for a single car by ID.
@@ -105,32 +145,84 @@ public class CarControllerTest {
      */
     @Test
     public void findCar() throws Exception {
-        /**
-         * TODO: Add a test to check that the `get` method works by calling
-         *   a vehicle by ID. This should utilize the car from `getCar()` below.
-         */
+    	
+    	Car actual = carService.findById(carId);
+		Car expected = carInstance;
+		log.info("city = {}", actual.getLocation().getCity());
+		log.info("car price = {}", actual.getPrice());
+		log.info("car model = {}", actual.getDetails().getModel());
+		log.info("Id = {}", actual.getId());
+		Assert.assertEquals(actual, expected);
+		
+    	String carIdAsStr = carId.toString();
+    	URI uri = new URI("/cars/" + carIdAsStr);
+    	
+    	log.info("Path Variable in find by Id = {}", uri.getPath());
+		
+		  mvc.perform(get(uri)
+		  .accept(MediaType.APPLICATION_JSON_UTF8)) 
+		  .andExpect(status().isOk())
+		  .andExpect(jsonPath("$.id", is(carId.intValue())))
+		  .andExpect(jsonPath("$.price", is("2000USD")))
+		  .andExpect(jsonPath("$.location.city", is("New York")))
+		  .andExpect(jsonPath("$.location.state", is("New York")))
+		  .andExpect(jsonPath("$.location.zip", is("1 200 200 111 NY")))
+		  .andExpect(jsonPath("$.condition", is(Condition.USED.name())));
+		 
     }
 
     /**
      * Tests the deletion of a single car by ID.
      * @throws Exception if the delete operation of a vehicle fails
      */
-    @Test
-    public void deleteCar() throws Exception {
-        /**
-         * TODO: Add a test to check whether a vehicle is appropriately deleted
-         *   when the `delete` method is called from the Car Controller. This
-         *   should utilize the car from `getCar()` below.
-         */
-    }
+	@Test
+	public void deleteCar() throws Exception {
+		
+		String carIdAsStr = carId.toString();
+		URI uri = new URI("/cars/" + carIdAsStr);
+
+		log.info("Path Variable in DELETE = {}", uri.getPath());
+		mvc.perform(delete(uri).accept(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isNoContent());
+	}
 
     /**
      * Creates an example Car object for use in testing.
      * @return an example Car object
      */
     private Car getCar() {
-        Car car = new Car();
+        Car car = getCarWithDetails();
+        
         car.setLocation(new Location(40.730610, -73.935242));
+        
+        return car;
+    }
+     
+    /**
+     * Creates an example Car object wth location and car id for use in testing.
+     * @return an example Car object wth location and car id
+     */
+    private Car getCar(Long id, Location location) {
+        Car car = getCar();
+        car.setId(id);
+      
+        String price = priceClient.getPrice(id);
+        log.info("price in = {}", price);
+        car.setPrice(price);
+        
+        Location address = mapsClient.getAddress(location);
+        log.info("city address in = {}", address.getCity());
+        car.setLocation(address);
+        
+        return car;
+    }
+    /**
+     * Creates an example car with details
+     * @return an example Car object
+     */
+    private Car getCarWithDetails() {
+        Car car = new Car();
+      
         Details details = new Details();
         Manufacturer manufacturer = new Manufacturer(101, "Chevrolet");
         details.setManufacturer(manufacturer);
@@ -147,4 +239,5 @@ public class CarControllerTest {
         car.setCondition(Condition.USED);
         return car;
     }
+  
 }
