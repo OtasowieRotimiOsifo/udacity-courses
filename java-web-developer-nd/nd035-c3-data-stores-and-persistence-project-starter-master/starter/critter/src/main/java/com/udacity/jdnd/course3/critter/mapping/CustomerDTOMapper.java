285d6f37280f61ad269d3386c660a2f051cb3fe0
Otasowie Osifo <47301269+OtasowieRotimiOsifo@users.noreply.github.com>
2022-01-09 21:59:40 +0100

    added plugin dependency for java  compilercomp

--- java-web-developer-nd/nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/mapping/CustomerDTOMapper.java
+++ java-web-developer-nd/nd035-c3-data-stores-and-persistence-project-starter-master/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/mapping/CustomerDTOMapper.java
@@ -3,7 +3,7 @@ package com.udacity.jdnd.course3.critter.mapping;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class Mapper {
+public class CustomerDTOMapper {
     @Autowired
     private ModelMapper modelMapper;
 
