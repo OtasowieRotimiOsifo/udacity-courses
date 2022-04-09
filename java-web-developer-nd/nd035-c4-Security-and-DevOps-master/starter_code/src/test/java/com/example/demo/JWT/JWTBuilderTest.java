68a78806ab47a0b32f1f37adf99f10a451853a78
Otasowie Osifo <Otasowie.Osifo@physics.org>
2022-04-09 15:42:11 +0200

    added tests for jwt build

--- java-web-developer-nd/nd035-c4-Security-and-DevOps-master/starter_code/src/test/java/com/example/demo/JWT/JWTBuilderTest.java
+++ java-web-developer-nd/nd035-c4-Security-and-DevOps-master/starter_code/src/test/java/com/example/demo/JWT/JWTBuilderTest.java
@@ -10,7 +10,26 @@ import org.junit.Test;
 
 public class JWTBuilderTest {
     @Test
-    public void validateJWT() throws Exception {
+    public void validateCreateJWT() throws Exception {
+        String jwt_token_secret = "dGhlX2FyZ29uYXV0cw==";
+        long jwt_time_to_live_ms = 3600000;
+        String userName = "test1";
+
+        JWTBuilder builder = new JWTBuilder(jwt_time_to_live_ms, jwt_token_secret);
+
+        String token = builder.buildToken(userName);
+
+        Assert.assertNotNull(token);
+
+        Algorithm algorithm =  Algorithm.HMAC512(jwt_token_secret.getBytes());
+        JWTVerifier verifier =  JWT.require(algorithm).build();
+        DecodedJWT decoded = verifier.verify(token);
+
+        Assert.assertNotNull(decoded);
+    }
+
+    @Test
+    public void validateJWTAlgorithm() throws Exception {
         String jwt_token_secret = "dGhlX2FyZ29uYXV0cw==";
         long jwt_time_to_live_ms = 3600000;
         String userName = "test1";
@@ -20,10 +39,31 @@ public class JWTBuilderTest {
 
         String token = builder.buildToken(userName);
 
+        Assert.assertNotNull(token);
+
         DecodedJWT decoded = JWT.decode(token);
-        String actual =  Algorithm.HMAC512(jwt_token_secret.getBytes()).getName();
+
+        String algorithm =  Algorithm.HMAC512(jwt_token_secret.getBytes()).getName();
         String expected = decoded.getAlgorithm();
 
-        Assert.assertEquals(actual.hashCode(), expected.hashCode());
+        Assert.assertEquals(algorithm.hashCode(), expected.hashCode());
+    }
+
+    @Test
+    public void validateJWTSubject() throws Exception {
+        String jwt_token_secret = "dGhlX2FyZ29uYXV0cw==";
+        long jwt_time_to_live_ms = 3600000;
+        String userName = "test1";
+
+        JWTBuilder builder = new JWTBuilder(jwt_time_to_live_ms, jwt_token_secret);
+
+        String token = builder.buildToken(userName);
+
+        Assert.assertNotNull(token);
+
+        DecodedJWT decoded = JWT.decode(token);
+        String subject = decoded.getSubject();
+
+        Assert.assertEquals(userName, subject);
     }
 }
