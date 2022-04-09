4be098b8010137ff4c37a22205d818b429abe54b
Otasowie Osifo <Otasowie.Osifo@physics.org>
2022-04-09 19:12:31 +0200

    added basic assertions to integration test

--- java-web-developer-nd/nd035-c4-Security-and-DevOps-master/starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
+++ java-web-developer-nd/nd035-c4-Security-and-DevOps-master/starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -1,16 +1,39 @@
 package com.example.demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.example.demo.controllers.CartController;
+
+import com.example.demo.controllers.ItemController;
+import com.example.demo.controllers.OrderController;
+import com.example.demo.controllers.UserController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SareetaApplicationTests {
 
+	@Autowired
+	private CartController cartController;
+
+	@Autowired
+	private ItemController itemController;
+
+	@Autowired
+	private OrderController orderController;
+
+	@Autowired
+	private UserController userController;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		Assertions.assertNotNull(cartController);
+		Assertions.assertNotNull(itemController);
+		Assertions.assertNotNull(orderController);
+		Assertions.assertNotNull(userController);
 	}
 
 }
--- java-web-developer-nd/nd035-c4-Security-and-DevOps-master/starter_code/src/test/java/com/example/demo/jwt/JWTBuilderTests.java
+++ java-web-developer-nd/nd035-c4-Security-and-DevOps-master/starter_code/src/test/java/com/example/demo/jwt/JWTBuilderTests.java
@@ -5,7 +5,6 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
-import com.example.demo.jwt.JWTBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
