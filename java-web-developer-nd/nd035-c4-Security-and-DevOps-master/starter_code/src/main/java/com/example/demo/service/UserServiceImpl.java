2ff3aa6a180292b2fc19251c8ef3a72c73d925be
Otasowie Osifo <Otasowie.Osifo@physics.org>
2022-04-09 23:43:21 +0200

    refactored the code

--- java-web-developer-nd/nd035-c4-Security-and-DevOps-master/starter_code/src/main/java/com/example/demo/service/UserServiceImpl.java
+++ java-web-developer-nd/nd035-c4-Security-and-DevOps-master/starter_code/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -14,7 +14,6 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService{
     private final PasswordEncoder bCryptPasswordEncoder;
 
-    //@Autowired
     private UserRepository userRepository;
 
     @Autowired
